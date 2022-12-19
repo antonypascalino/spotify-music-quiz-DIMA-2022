0bd3fee5fc765c3c9237595c207dbe1c67c5a1b8
antonypascalino <antony.pascalino@mail.polimi.it>
2022-12-19 15:27:18 +0100

    Initial Commit

---
+++ spotify-music-quiz-mida-2022/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  spotify-music-quiz-mida-2022
+//
+//  Created by Antony Pascalino on 19/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ spotify-music-quiz-mida-2022/spotify_music_quiz_mida_2022App.swift
@@ -0,0 +1,17 @@
+//
+//  spotify_music_quiz_mida_2022App.swift
+//  spotify-music-quiz-mida-2022
+//
+//  Created by Antony Pascalino on 19/12/22.
+//
+
+import SwiftUI
+
+@main
+struct spotify_music_quiz_mida_2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
