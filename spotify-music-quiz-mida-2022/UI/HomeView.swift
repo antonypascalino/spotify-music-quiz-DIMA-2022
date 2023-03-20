834f6ad36ca10d85fd7bfa3a399cd6f784ccf171
antonypascalino <antony.pascalino@mail.polimi.it>
2023-03-20 11:54:02 +0100

    Completed the HomeView

--- spotify-music-quiz-mida-2022/UI/HomeView.swift
+++ spotify-music-quiz-mida-2022/UI/HomeView.swift
@@ -8,33 +8,63 @@
 import SwiftUI
 
 let name = "Antony"
+let score = 257
 
 struct HomeView: View {
     var body: some View {
-        ScrollView {
-            HStack {
-                Text("Hi \(name)!")
-                    .font(TextStyle.homeTitle())
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding()
-                    .foregroundColor(.white)
-                Spacer()
-                Image(systemName: "bell")
-                    .font(.system(size: 20, weight: .bold))
-                    .foregroundColor(.white)
-                    .frame(width: 50, height: 50)
-                NavigationLink(destination: SettingsView()) {
-                    Image(systemName: "gearshape")
+        
+            VStack {
+                HStack {
+                    Text("Hi \(name)!")
+                        .font(TextStyle.homeTitle())
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding()
+                        .foregroundColor(.white)
+                    Spacer()
+                    Image(systemName: "bell")
                         .font(.system(size: 20, weight: .bold))
                         .foregroundColor(.white)
                         .frame(width: 50, height: 50)
+                    NavigationLink(destination: SettingsView()) {
+                        Image(systemName: "gearshape")
+                            .font(.system(size: 20, weight: .bold))
+                            .foregroundColor(.white)
+                            .frame(width: 50, height: 50)
+                    }
                 }
-                .navigationTitle("Settings")
+                Spacer()
+                Image("Profilo1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 250.0, height: 250.0)
+                    .clipShape(Circle())
+                Spacer()
+                Text("Your highscore:")
+                    .font(TextStyle.scoreTitle())
+                    .foregroundColor(Color("Green"))
+                    .padding(.bottom)
+                Text(String(score))
+                    .font(TextStyle.score())
+                    .foregroundColor(Color("Green"))
+                    .padding(.bottom, 40.0)
+                NavigationLink(destination: GameView()) {
+                    Image("GreenPlay")
+                        .resizable()
+                        .frame(width: 100.0, height: 100.0)
+                }
             }
-        }
-        .background(Color("Black"))
+            .background(
+                RadialGradient(gradient: Gradient(colors: [Color("Green"), Color("Black")]),
+                               center: UnitPoint(x: 0.50, y: 0.35),
+                               startRadius: 20,
+                               endRadius: 380)
+                
+            )
+            .navigationTitle("Home")
+            .foregroundColor(.white)
     }
 }
+    
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
