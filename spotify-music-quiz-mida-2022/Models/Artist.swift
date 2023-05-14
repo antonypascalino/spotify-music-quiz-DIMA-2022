96f51d1efa665ab5d4daeaf9cafcd1fa801d24ab
antonypascalino <antony.pascalino@mail.polimi.it>
2023-05-14 20:05:10 +0200

    Settled the files

---
+++ spotify-music-quiz-mida-2022/Models/Artist.swift
@@ -0,0 +1,17 @@
+//
+//  Artist.swift
+//  spotify-music-quiz-mida-2022
+//
+//  Created by Antony Pascalino on 08/05/23.
+//
+
+import Foundation
+
+struct Artist: Codable {
+    let id : String
+    let name : String
+    //let type : String
+    //let external_urls : [String : String]
+    //let images : [APIImage]?
+    
+}
---
+++ spotify-music-quiz-mida-2022/Models/Item.swift
@@ -0,0 +1,12 @@
+//
+//  Item.swift
+//  spotify-music-quiz-mida-2022
+//
+//  Created by Antony Pascalino on 08/05/23.
+//
+
+import Foundation
+
+struct Item: Codable {
+    let item : [Artist]?
+}
