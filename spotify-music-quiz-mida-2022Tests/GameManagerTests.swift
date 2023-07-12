42523bb45c0bdd75b5fc703616a246d82e9c8515
antonypascalino <antony.pascalino@mail.polimi.it>
2023-07-13 01:43:31 +0200

    Added wait for the test

--- spotify-music-quiz-mida-2022Tests/GameManagerTests.swift
+++ spotify-music-quiz-mida-2022Tests/GameManagerTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import spotify_music_quiz_mida_2022 // Importa il nome corretto della tua app
 
 class GameManagerTests: XCTestCase {
-
+    
     var gameManager: MockGameManager!
     
     override func setUp() {
@@ -74,9 +74,7 @@ class GameManagerTests: XCTestCase {
         }
     }
     
-    // Aggiungi altri test secondo le tue esigenze
     func testSetNextQuestion() async throws {
-        // Assicurati che l'array delle domande sia vuoto prima di eseguire questo test
         
         gameManager.questions = []
         gameManager.tempQuestions = []
@@ -86,68 +84,70 @@ class GameManagerTests: XCTestCase {
         do {
             try await gameManager.startGame(codeQuestion: codeQuestion)
             
-            // Verifica che l'array delle domande sia stato generato e che la domanda corrente sia impostata correttamente
             XCTAssertFalse(gameManager.questions.isEmpty)
             XCTAssertEqual(gameManager.currentQuestionIndex, 0)
             XCTAssertNotNil(gameManager.currentQuestion)
             XCTAssertNotNil(gameManager.currentAnswers)
             
-            // Simula la risposta corretta alla domanda corrente
             gameManager.selectAnswer(true)
             
-            
-            // Verifica che la risposta sia stata conteggiata come corretta
             XCTAssertEqual(gameManager.correctAnswersCount, 1)
             
             try await gameManager.setNextQuestion()
-
-           //wait(for: )
-
             
-            // Verifica che la domanda corrente sia passata alla successiva
-            XCTAssertEqual(gameManager.currentQuestionIndex, 1)
-            XCTAssertNotNil(gameManager.currentQuestion)
-            XCTAssertNotNil(gameManager.currentAnswers)
+            await waitForExpectations(timeout: 5) { error in
+                if let error = error {
+                    XCTFail("Timeout: \(error)")
+                } else {
+                    XCTAssertEqual(self.gameManager.currentQuestionIndex, 1)
+                    XCTAssertNotNil(self.gameManager.currentQuestion)
+                    XCTAssertNotNil(self.gameManager.currentAnswers)
+                }
+            }
             
-            // Simula la risposta corretta alla domanda corrente
+            
             gameManager.selectAnswer(true)
             
-
             
-            // Verifica che la risposta sia stata conteggiata come corretta
+            
             XCTAssertEqual(gameManager.correctAnswersCount, 2)
             
             try await gameManager.setNextQuestion()
             
-            //wait(for: 5)
-
-
-            // Verifica che la domanda corrente sia passata alla successiva
-            XCTAssertEqual(gameManager.currentQuestionIndex, 2)
-            XCTAssertNotNil(gameManager.currentQuestion)
-            XCTAssertNotNil(gameManager.currentAnswers)
+            await waitForExpectations(timeout: 5) { error in
+                if let error = error {
+                    XCTFail("Timeout: \(error)")
+                } else {
+                    XCTAssertEqual(self.gameManager.currentQuestionIndex, 2)
+                    XCTAssertNotNil(self.gameManager.currentQuestion)
+                    XCTAssertNotNil(self.gameManager.currentAnswers)
+                }
+            }
             
-            // Simula la risposta corretta alla domanda corrente
+            
+            
             gameManager.selectAnswer(true)
             
-            // Verifica che la risposta sia stata conteggiata come corretta
             XCTAssertEqual(gameManager.correctAnswersCount, 3)
             
             try await gameManager.setNextQuestion()
             
-            //wait(for: 5)
-
-
-            // Verifica che tutte le domande siano state completate e che la generazione di nuove domande sia avvenuta correttamente
-            XCTAssertEqual(gameManager.currentQuestionIndex, 0)
-            XCTAssertNotNil(gameManager.currentQuestion)
-            XCTAssertNotNil(gameManager.currentAnswers)
-            XCTAssertTrue(gameManager.questions.isEmpty)
-            XCTAssertFalse(gameManager.tempQuestions.isEmpty)
-        } catch {
-            XCTFail("Unexpected error: \(error)")
+            await waitForExpectations(timeout: 5) { error in
+                if let error = error {
+                    XCTFail("Timeout: \(error)")
+                } else {
+                    XCTAssertEqual(self.gameManager.currentQuestionIndex, 0)
+                    XCTAssertNotNil(self.gameManager.currentQuestion)
+                    XCTAssertNotNil(self.gameManager.currentAnswers)
+                    XCTAssertTrue(self.gameManager.questions.isEmpty)
+                    XCTAssertFalse(self.gameManager.tempQuestions.isEmpty)
+                }
+            }
+            
+            
+            
         }
+        
     }
-
+    
 }
-
